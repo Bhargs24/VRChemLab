29c682f1a236065cc0f3a988faa3b944ef75c5ad
Bhargav R <bhargavrag16@gmail.com>
2024-09-06 11:33:15 +0530

    Fixed salt visuals and added attach transforms

--- Assets/Scripts/Dish.cs
+++ Assets/Scripts/Dish.cs
@@ -3,24 +3,32 @@ using UnityEngine;
 public class Dish : MonoBehaviour
 {
     public Transform saltSpawnPoint;
-    public GameObject saltPrefab;
+    private GameObject saltPrefab;
     private float totalSaltWeight = 0f;
 
+    private void Awake()
+    {
+        saltPrefab = this.transform.Find("salt").gameObject;
+    }
     public void AddSalt(float saltAmount)
     {
         totalSaltWeight += saltAmount;
-        InstantiateSaltVisual(saltAmount);
+        InstantiateSaltVisual(totalSaltWeight);
     }
 
     private void InstantiateSaltVisual(float saltAmount)
     {
-        if (saltPrefab != null && saltSpawnPoint != null)
+        /*if (saltPrefab != null && saltSpawnPoint != null)
         {
-            GameObject saltVisual = Instantiate(saltPrefab, saltSpawnPoint.position, Quaternion.identity);
+            GameObject saltVisual = Instantiate(saltPrefab, saltSpawnPoint.localPosition, Quaternion.identity);
             float scaleMultiplier = saltAmount / 5f;
             saltVisual.transform.localScale *= scaleMultiplier;
             saltVisual.transform.SetParent(saltSpawnPoint);
-        }
+        }*/
+        saltPrefab.SetActive(true);
+        // float scaleFactor = totalSaltWeight;
+        saltPrefab.transform.localScale *= totalSaltWeight;
+
     }
 
     public float GetSaltWeight()
--- Assets/Scripts/Spatula.cs
+++ Assets/Scripts/Spatula.cs
@@ -2,11 +2,17 @@ using UnityEngine;
 
 public class Spatula : MonoBehaviour
 {
-    public GameObject saltPrefab;
+    public GameObject Dishobj;
+    private GameObject saltPrefab;
     public Transform saltSpawnPoint;
     public float saltAmount = 5f;
     private bool hasSalt = false;
-
+    private Dish dishScript;
+    private void Awake()
+    {
+        dishScript = Dishobj.GetComponent<Dish>();
+        saltPrefab = this.transform.Find("salt").gameObject;
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "SaltBottle" && !hasSalt)
@@ -15,24 +21,37 @@ public class Spatula : MonoBehaviour
         }
         else if (collision.gameObject.name == "Dish" && hasSalt)
         {
+            print("collide");
             TransferSaltToDish(collision.gameObject);
         }
     }
 
     private void PickupSalt()
     {
-        if (saltPrefab != null && saltSpawnPoint != null)
+
+        /* if (saltPrefab != null && saltSpawnPoint != null)
         {
-            Instantiate(saltPrefab, saltSpawnPoint.position, Quaternion.identity, transform);
+            
             hasSalt = true;
         }
+        
+    
+        }
+        */
+        if(!hasSalt)
+        {
+            saltPrefab.SetActive(true);    
+            hasSalt = true;
+
+        }
+
     }
 
     private void TransferSaltToDish(GameObject dish)
     {
-        foreach (Transform child in transform)
+        /*foreach (Transform child in transform)
         {
-            if (child.gameObject.name == "Salt")
+            if (child.gameObject.name == "salt")
             {
                 Destroy(child.gameObject);
                 hasSalt = false;
@@ -42,6 +61,10 @@ public class Spatula : MonoBehaviour
                 }
                 break;
             }
-        }
+        }*/
+        print("collision detected");
+        saltPrefab.SetActive(false);
+        hasSalt = false;
+        dishScript.AddSalt(saltAmount);
     }
 }
