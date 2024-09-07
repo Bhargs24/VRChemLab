2c40e5d68f0b3779bccc9deee336f7090786de54
Chaos1703 <147336231+Chaos1703@users.noreply.github.com>
2024-09-07 22:14:13 +0530

    Respawning and dish fixed

--- Assets/Scripts/WeighingBalance.cs
+++ Assets/Scripts/WeighingBalance.cs
@@ -3,66 +3,86 @@ using UnityEngine.UI;
 
 public class WeighingBalance : MonoBehaviour
 {
-    public Transform snapPosition;  
-    public Text weightDisplay;      
-    public Button tareButton;       
-    public Button changeUnitButton; 
-    public Button onOffButton;      
-
-    private float currentWeight = 0f;  
-    private bool isBalanceOn = true;   
-    private float tareWeight = 0f;     
-    private string currentUnit = "g";  
+    private Vector3 snapPosition;
+    public Text weightDisplay;
+    public Button tareButton;
+    public Button changeUnitButton;
+    public Button onOffButton;
+    private float currentWeight = 0f;
+    private bool isBalanceOn = true;
+    private float tareWeight = 0f;
+    private string currentUnit = "g";
 
     private void Start()
     {
-        
+        snapPosition = new Vector3(0.139799997f, 0.647000015f, -3.68799996f);
         tareButton.onClick.AddListener(TareScale);
         changeUnitButton.onClick.AddListener(ChangeUnit);
         onOffButton.onClick.AddListener(ToggleBalance);
     }
 
-    private void OnCollisionEnter(Collision other)
+    void OnTriggerEnter(Collider other)
     {
-        
-        if (other.gameObject.CompareTag("Dish"))
+        Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+        if (other.gameObject.tag == "Dish")
         {
-           
-            other.transform.position = snapPosition.position;
-
-            
-            Dish dish = other.gameObject.GetComponent<Dish>();
-            if (dish != null)
-            {
-                currentWeight = dish.GetSaltWeight() - tareWeight;
-                UpdateWeightDisplay();
-            }
+            rb.useGravity = false;
+            rb.isKinematic = true;
+            other.gameObject.transform.position = snapPosition;
         }
     }
 
+    void OnTriggerExit(Collider other)
+    {
+        Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+        if (other.gameObject.tag == "Dish")
+        {
+            rb.useGravity = true;
+            rb.isKinematic = false;
+        }
+    }
+
+    // private void OnCollisionEnter(Collision other)
+    // {
+
+    //     if (other.gameObject.CompareTag("Dish"))
+    //     {
+
+    //         other.transform.position = snapPosition.position;
+
+
+    //         Dish dish = other.gameObject.GetComponent<Dish>();
+    //         if (dish != null)
+    //         {
+    //             currentWeight = dish.GetSaltWeight() - tareWeight;
+    //             UpdateWeightDisplay();
+    //         }
+    //     }
+    // }
+
     private void TareScale()
     {
-       
+
         tareWeight = currentWeight;
         UpdateWeightDisplay();
     }
 
     private void ChangeUnit()
     {
-        
+
         switch (currentUnit)
         {
             case "g":
                 currentUnit = "kg";
-                currentWeight /= 1000f;  
+                currentWeight /= 1000f;
                 break;
             case "kg":
                 currentUnit = "mg";
-                currentWeight *= 1000f * 1000f;  
+                currentWeight *= 1000f * 1000f;
                 break;
             case "mg":
                 currentUnit = "g";
-                currentWeight /= 1000f;  
+                currentWeight /= 1000f;
                 break;
         }
         UpdateWeightDisplay();
@@ -70,14 +90,14 @@ public class WeighingBalance : MonoBehaviour
 
     private void ToggleBalance()
     {
-        
+
         isBalanceOn = !isBalanceOn;
         weightDisplay.text = isBalanceOn ? currentWeight.ToString("F2") + " " + currentUnit : "0";
     }
 
     private void UpdateWeightDisplay()
     {
-        
+
         if (isBalanceOn)
         {
             weightDisplay.text = currentWeight.ToString("F2") + " " + currentUnit;
