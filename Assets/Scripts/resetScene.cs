3f7ab46c7c3dcc0bd4d2709b525786879d9dde5a
Chaos1703 <147336231+Chaos1703@users.noreply.github.com>
2024-09-12 17:20:35 +0530

    resest and spoon done

--- Assets/Scripts/resetScene.cs
+++ Assets/Scripts/resetScene.cs
@@ -4,17 +4,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class resetScene : MonoBehaviour
 {
-    public float pressThreshold = 0.05f;
-    private Vector3 initialPosition;
 
-    void Start()
+    void OnTriggerEnter(Collider other)
     {
-        initialPosition = transform.localPosition;
-    }
-    void Update()
-    {
-        // Check if the button is pressed
-        if (Mathf.Abs(transform.localPosition.z - initialPosition.z) > pressThreshold)
+        if (other.gameObject.tag == "Left Hand" || other.gameObject.tag == "Right Hand")
         {
             ResetCurrentScene();
         }
